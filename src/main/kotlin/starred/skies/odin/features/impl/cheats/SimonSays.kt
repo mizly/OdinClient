723f2af702cb930f4ded8365c7589555600fa285
skies-starred <skies.starred@gmail.com>
2026-02-10 23:49:46 +0530

    fix(SimonSays): auto start not working, hopefully.

--- src/main/kotlin/starred/skies/odin/features/impl/cheats/SimonSays.kt
+++ src/main/kotlin/starred/skies/odin/features/impl/cheats/SimonSays.kt
@@ -3,15 +3,14 @@ package starred.skies.odin.features.impl.cheats
 import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
 import com.odtheking.odin.clickgui.settings.impl.*
 import com.odtheking.odin.events.ChatPacketEvent
+import com.odtheking.odin.events.TickEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.features.Module
-import com.odtheking.odin.utils.handlers.schedule
 import net.minecraft.core.BlockPos
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.HitResult
 import starred.skies.odin.utils.Skit
 import starred.skies.odin.utils.rightClick
-import kotlin.random.Random.Default.nextInt
 
 object SimonSays : Module(
     name = "Simon Says Additions",
@@ -23,9 +22,28 @@ object SimonSays : Module(
     private val startClicks by NumberSetting("Start Clicks", 3, 1, 10, desc = "Amount of clicks to start the device.").withDependency { autoStart }
     private val startClickDelay by NumberSetting("Start Click Delay", 3, 1, 25, unit = "ticks", desc = "Delay between each start click.").withDependency { autoStart }
 
+    private var clicksLeft = 0
+    private var delayTicks = 0
+    private var active = false
+
     init {
         on<ChatPacketEvent> {
             if (value == "[BOSS] Goldor: Who dares trespass into my domain?") s()
+        }
+
+        on<TickEvent.Start> {
+            if (!active) return@on
+            if (mc.screen != null) return@on
+
+            if (delayTicks > 0) {
+                delayTicks--
+                return@on
+            }
+
+            rightClick()
+            clicksLeft--
+
+            if (clicksLeft <= 0) active = false else delayTicks = startClickDelay
         }
     }
 
@@ -33,10 +51,8 @@ object SimonSays : Module(
         val h = mc.hitResult?.takeIf { it.type == HitResult.Type.BLOCK } ?: return
         if ((h as? BlockHitResult)?.blockPos != startButton) return
 
-        var i = 0
-        while (i < startClicks) {
-            schedule(i * startClickDelay + (2 + nextInt(2))) { rightClick() }
-            i++
-        }
+        clicksLeft = startClicks
+        delayTicks = 0
+        active = true
     }
 }
